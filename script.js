57b0c7da4fbcd326bddcdf1ed27d60b891169d0f
unknown <Abike Ladele>
2022-05-25 15:11:11 +0100

    JavaScript trial

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('I am coming from the internal script in index.html to the external script of script.js')
+
+//This is JavaScript single line comment
+//Comments make your code more readable
+/*
+This is the comment used for multiple lines
+Here comments can go in multiple lines
+Do you not just appreciate how smooth and readable your code looks?
+*/
+
+console.log ('This is a text I want to print')
+console.log('item 1', 'item 2', 'item 3', 'COVID-19')
+
+// Data types in JavaScript: Number, String, Boolean, Null, Undefined
+
+//Numbers
+console.log(15)
+console.log (3 + 5)
+console.log (3 - 5)
+console.log (3 * 5)
+console.log (21 % 4)
+console.log (5 * 5 * 5)
+console.log (5 ** 3) 
+console.log(2 + 2 == 2 * 2)
